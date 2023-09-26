4e5587f191be80c3cee0bb481dac644746fd5908
Dadmehr <garfoxteam@gmail.com>
2023-09-26 11:33:42 +0330

    pages

--- main.py
+++ main.py
@@ -1,7 +1,75 @@
 import customtkinter as ctk
 import tkinter as tk
 
-class MAIN(ctk.CTk):
+from PIL import Image, ImageTk
+
+# Pages
+class main(ctk.CTkFrame):
++ 
++     def __init__(self, parent, controller):
++         ctk.CTkFrame.__init__(self, parent)
++         self.controller = controller
++         self.new_frame_status = False
++ 
++ 
++ 
++         # Full Screen Code
++         pad=3
++         self._geom='500x600+0+0'
+         controller.geometry("{0}x{1}+0+0".format(
+            controller.winfo_screenwidth()-pad, controller.winfo_screenheight()-pad))
+        controller.bind('<F11>',self.togg)
+class main(ctk.CTkFrame):
+
+    def __init__(self, parent, controller):
+        ctk.CTkFrame.__init__(self, parent)
+        self.controller = controller
+        self.new_frame_status = False
+
+
+
+        # Full Screen Code
+        pad=3
+        self._geom='500x600+0+0'
+        controller.geometry("{0}x{1}+0+0".format(
+            controller.winfo_screenwidth()-pad, controller.winfo_screenheight()-pad))
+        controller.bind('<F11>',self.toggle_geom)
+
+class WelcomePage(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title('Welcome')
+        self.geometry('400x300')
+
+        p1 = tk.PhotoImage(file="./assets/logo/favicon/opengmaezone-website-favicon-color.png")
+        self.iconphoto(False, p1)
+
+        self.image = Image.open("./assets/logo/favicon/opengmaezone-website-favicon-color.png")
+
+        new_width = 200
+        new_height = 150
+        self.image = self.image.resize((new_width, new_height), Image.ANTIALIAS)
+
+        self.photo = ImageTk.PhotoImage(self.image)
+
+        self.label = ctk.CTkLabel(self, image=self.photo, text='')
+        self.label.pack(pady=20)
+
+        self.welcome_label = ctk.CTkLabel(self, text='Welcome to the app!', font=('roboto', 25))
+        self.welcome_label.pack()
+
+        self.after(5000, self.close_welcome)
+
+    def close_welcome(self):
+        self.destroy()
+        self.open_main_app()
+
+    def open_main_app(self):
+        app = Frame_ch()
+        app.mainloop()
+
+# Frame Changer
+class Frame_ch(ctk.CTk):
     def __init__(self):
         ctk.CTk.__init__(self)
         container = ctk.CTkFrame(self)
@@ -32,5 +100,5 @@ class MAIN(ctk.CTk):
         frame.tkraise()
 
 if __name__ == "__main__":
-    # welcome_page = WelcomePage()
+    welcome_page = WelcomePage()
     welcome_page.mainloop()
